a4af4e49b2c31c42825eea99e4a498399bfc3e39
kristina uko <kristinaukosina@gmail.com>
2019-05-25 10:42:03 -0700

    day 36: proposed solution

--- day36.py
+++ day36.py
@@ -61,6 +61,29 @@ def max(stack):
     return maxim 
 
 
+#Proposed Solution
+
+# class MaxStack:
+#     def __init__(self):
+#         self.stack = []
+#         self.maxes = []
+
+#     def push(self, val):
+#         self.stack.append(val)
+#         if self.maxes:
+#             self.maxes.append(max(val, self.maxes[-1]))
+#         else:
+#             self.maxes.append(val)
+
+#     def pop(self):
+#         if self.maxes:
+#             self.maxes.pop()
+#         return self.stack.pop()
+
+#     def max(self):
+#         return self.maxes[-1]
+
+
 if __name__ == "__main__":
     import doctest
 
